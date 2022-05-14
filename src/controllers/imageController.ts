637f856526fbcf1c0664729d8bbdc1431d23b2fe
devanshkaushik <devanshkaushik0711@gmail.com>
2022-05-14 15:55:10 +0530

    Added multer to get posted images

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 
 export const image_detect_object = (req: Request, res: Response) => {
+  console.log(req.file)
   res.send("NOT IMPLEMENTED: Image detect object")
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
 import { image_detect_object } from "./controllers/imageController"
+import { upload } from "./middlewares/uploadMiddleware"
 
 const router = Router()
 
-router.post("/", image_detect_object)
+router.post("/", upload.single("image"), image_detect_object)
 
 export default router
